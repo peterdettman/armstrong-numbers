74a543e8de62196078fac3352ade356a97b8c9a8
Peter Dettman <peter.dettman@gmail.com>
2021-03-18 13:17:36 +0700

    Log intermediate elapsed times

--- src/armstrong/Armstrong.java
+++ src/armstrong/Armstrong.java
@@ -6,29 +6,27 @@ public class Armstrong {
 
     public static void main(String[] args) throws Exception {
 
-        long before = System.nanoTime();
+        long startTime = System.nanoTime();
 
         if (args.length > 0) {
             int length = Integer.parseInt(args[0]);
             if (length > 0) {
-                generateSolutions(length);
+                generateSolutions(startTime, length);
             }
         } else {
             for (int length = 1; length <= 60; ++length) {
-                generateSolutions(length);
+                generateSolutions(startTime, length);
             }
         }
 
-        long after = System.nanoTime();
-        long elapsed = TimeUnit.MILLISECONDS.convert(after - before, TimeUnit.NANOSECONDS);
-        System.out.println("Elapsed: " + elapsed + "ms");
+        System.out.println("Elapsed: " + getElapsed(startTime) + "ms");
     }
 
-    private static void generateSolutions(int length) {
+    private static void generateSolutions(long startTime, int length) {
 
         assert length >= 1;
 
-        System.out.println("Length " + length);
+        System.out.println("Length " + length + " [@" + getElapsed(startTime) + "ms]");
 
         if (length == 1) {
             for (int digit = 0; digit <= 9; ++digit) {
@@ -38,5 +36,9 @@ public class Armstrong {
         else if (length <= 60) {
             Solver.forLength(length).generateSolutions();
         }
+    }
+
+    private static long getElapsed(long startTime) {
+        return TimeUnit.MILLISECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
     }
 }
